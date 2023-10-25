117d48b25c630ee5656951d0ee6221a672cda949
Raj Nakarja <raj@siliconwitchery.com>
2023-10-25 20:54:25 +0200

    All the functions added

--- frameutils/bluetooth.py
+++ frameutils/bluetooth.py
@@ -1,45 +1,77 @@
 import asyncio
-import sys
-import os
 
 from bleak import BleakClient, BleakScanner
-from bleak.backends.characteristic import BleakGATTCharacteristic
-from bleak.backends.device import BLEDevice
-from bleak.backends.scanner import AdvertisementData
-from bleak.uuids import register_uuids
 
 
 class Bluetooth:
+    """
+    Frame bluetooth class for managing a connection and transferring data to and
+    from the device.
+    """
+
     _SERVICE_UUID = "7a230001-5475-a6a4-654c-8431f6ad49c4"
     _TX_CHARACTERISTIC_UUID = "7a230002-5475-a6a4-654c-8431f6ad49c4"
     _RX_CHARACTERISTIC_UUID = "7a230003-5475-a6a4-654c-8431f6ad49c4"
 
     def __init__(self):
         self._awaiting_response = False
+        self._client = None
         self._response_data = bytearray()
+        self._tx_characteristic = None
+        self._user_data_response_handler = lambda: None
+        self._user_disconnect_handler = lambda: None
+        self._user_lua_response_handler = lambda: None
 
-    def _filter_uuid(self, _: BLEDevice, adv: AdvertisementData):
+    def _filter_uuid(self, _, adv):
         return self._SERVICE_UUID in adv.service_uuids
 
-    def _disconnect_handler(self, _: BleakClient):
+    def _disconnect_handler(self, _):
         self._user_disconnect_handler()
+        self.__init__()
 
-    async def _notification_handler(self, _: BleakGATTCharacteristic, data: bytearray):
-        self._awaiting_response = False
-        self._response_data = data
+    async def _notification_handler(self, _, data):
+        if self._awaiting_response:
+            self._awaiting_response = False
+            self._response_data = data
+        else:
+            if data[0] == 1:
+                self._user_data_response_handler(data[1:])
+            else:
+                self._user_lua_response_handler(data.decode())
 
-    async def connect(self, disconnect_handler: callable = lambda: None):
+    async def connect(
+        self,
+        lua_response_handler: callable,
+        data_response_handler: callable,
+        disconnect_handler=lambda: None,
+    ):
+        """
+        Connects to the nearest Frame device.
+
+        `lua_response_handler` will be called whenever string data is received
+        from the device, and `data_response_handler` will be called whenever raw
+        data is received from the device.
+
+        Optionally, a `disconnect_handler` can be provided to be called to run
+        upon a disconnect.
+        """
         self._user_disconnect_handler = disconnect_handler
+        self._user_lua_response_handler = lua_response_handler
+        self._user_data_response_handler = data_response_handler
 
-        device = await BleakScanner.find_device_by_filter(self._filter_uuid)
+        device = await BleakScanner.find_device_by_filter(
+            self._filter_uuid,
+        )
 
         if device is None:
-            raise Exception("no Frame devices found")
+            raise Exception("no devices found")
 
         self._client = BleakClient(
             device,
             disconnected_callback=self._disconnect_handler,
         )
+
+        # TODO find a way to connect to the closest device (highest RSSI)
 
         await self._client.connect()
 
@@ -48,25 +80,87 @@ class Bluetooth:
             self._notification_handler,
         )
 
-        service = self._client.services.get_service(self._SERVICE_UUID)
-
-        self._tx_characteristic = service.get_characteristic(
-            self._TX_CHARACTERISTIC_UUID
+        service = self._client.services.get_service(
+            self._SERVICE_UUID,
         )
 
-    async def send_lua(self, string: str):
-        await self._client.write_gatt_char(self._tx_characteristic, string.encode())
-
-        self._awaiting_response = True
-        countdown = 1000
-
-        while self._awaiting_response:
-            await asyncio.sleep(0.001)
-            if countdown == 0:
-                raise Exception("Frame device didn't respond")
-            countdown -= 1
-
-        return self._response_data.decode()
+        self._tx_characteristic = service.get_characteristic(
+            self._TX_CHARACTERISTIC_UUID,
+        )
 
     async def disconnect(self):
+        """
+        Disconnects from the device.
+        """
         await self._client.disconnect()
+        self._disconnect_handler(None)
+
+    def is_connected(self):
+        """
+        Returns `True` if the device is connected. `False` otherwise.
+        """
+        try:
+            return self._client.is_connected
+        except AttributeError:
+            return False
+
+    def max_lua_payload(self):
+        """
+        Returns the maximum length of a Lua string which may be transmitted.
+        """
+        try:
+            return self._client.mtu_size - 3
+        except AttributeError:
+            return 0
+
+    def max_data_payload(self):
+        """
+        Returns the maximum length of a raw bytearray which may be transmitted.
+        """
+        try:
+            return self._client.mtu_size - 4
+        except AttributeError:
+            return 0
+
+    async def _transmit(self, data, show_me=False):
+        # TODO make this print nicer
+        if show_me:
+            print(data)
+
+        if len(data) > self._client.mtu_size - 3:
+            raise Exception("payload length is too large")
+
+        await self._client.write_gatt_char(self._tx_characteristic, data)
+
+    async def send_lua(self, string: str, show_me=False, wait=False):
+        """
+        Sends a Lua string to the device. The string length must be less than or
+        equal to `max_lua_payload()`.
+
+        If `wait=True`, the response will be returned. This is useful for Lua
+        `print()` statements.
+
+        If `show_me=True`, the exact bytes send to the device will be printed.
+        """
+        await self._transmit(string.encode(), show_me=show_me)
+
+        if wait:
+            self._awaiting_response = True
+            countdown = 2000
+
+            while self._awaiting_response:
+                await asyncio.sleep(0.001)
+                if countdown == 0:
+                    raise Exception("device didn't respond")
+                countdown -= 1
+
+            return self._response_data.decode()
+
+    async def send_data(self, data: bytearray, show_me: bool = False):
+        """
+        Sends raw data to the device. The payload length must be less than or
+        equal to `max_data_payload()`.
+
+        If `show_me=True`, the exact bytes send to the device will be printed.
+        """
+        await self._transmit(bytearray(b"\x01") + data, show_me=show_me)
--- main.py
+++ main.py
@@ -6,19 +6,55 @@ def disconnect_handler():
     print("Disconnected user callback!")
 
 
+def lua_response_handler(string):
+    print(f"Response: {string}")
+
+
+def data_response_handler(data):
+    print(f"Response: {data}")
+
+
 async def main():
     bluetooth = Bluetooth()
-    await bluetooth.connect()
 
-    print(await bluetooth.send_lua("print('hello')"))
+    print(bluetooth.is_connected())
+
+    await bluetooth.connect(
+        lua_response_handler,
+        data_response_handler,
+    )
+
+    print(bluetooth.is_connected())
+    print(bluetooth.max_lua_payload())
+    print(bluetooth.max_data_payload())
+
+    print(await bluetooth.send_lua("print('hello')", show_me=True, wait=True))
     await asyncio.sleep(1)
 
-    print(await bluetooth.send_lua("print('world')"))
+    print(await bluetooth.send_lua("print('world')", wait=True))
     await asyncio.sleep(1)
 
-    print(await bluetooth.send_lua("print(4 + 6)"))
-    print(await bluetooth.send_lua("print(6 + 6)"))
+    await bluetooth.send_lua("a = 5")
+    await asyncio.sleep(1)
+
+    print(await bluetooth.send_lua("print(a + 6)", wait=True))
+    print(await bluetooth.send_lua("print(6 + 6)", wait=True))
     await asyncio.sleep(3)
+
+    await bluetooth.disconnect()
+
+    print(bluetooth.is_connected())
+    await bluetooth.connect(
+        lua_response_handler,
+        data_response_handler,
+        disconnect_handler,
+    )
+
+    print(await bluetooth.send_lua("print('hi again')", show_me=True, wait=True))
+    await asyncio.sleep(1)
+
+    await bluetooth.send_data(bytearray(b"abc"), show_me=True)
+    await asyncio.sleep(1)
 
     await bluetooth.disconnect()
 
